93cd97a17d95fb0b3aac597f19236f32951edd66
johnnylu3 <lujohnny517@gmail.com>
2022-12-15 10:16:37 -0500

    1

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -4,8 +4,19 @@ import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
 import { drinks } from "./drinks";
+
 document.querySelector("#app").innerHTML = `
   <h1>Hello?????</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
 console.log(drinks);
+
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("cool")) {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+  } else {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+  }
+});
