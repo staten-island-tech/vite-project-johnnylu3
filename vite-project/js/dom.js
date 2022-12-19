e2a79ab867a56ead110ba2a69e98bd8e90825eae
johnnylu3 <lujohnny517@gmail.com>
2022-12-19 10:16:21 -0500

    41

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -6,3 +6,17 @@ const DOMSelectors = {
   input: document.querySelectorAll(".textbox"),
 };
 export { DOMSelectors };
+
+Data.filter((e)=> e.type.includes(meat)
+.forEach((e)=> {console.log(e.name)})
+DOM.box.insertadjacentHTML=
+{`<h1>${e.name}</h1>`
+});
+
+destinations.forEach((destination)=>{DOMSelectors.displaycontainer.insertAdjacentHTML("afterbegin", `<div class="menu-card"> <h2 class="menu-item"></h2> <div class="img-container">  <div class="img-overlay"></div> <img src="./public/.jpg" alt="" class="menu-img"> </div> <h3 class="item-price"></h3>
+<button class="btn">$${destination}</button></div>`)} )
+
+
+const title= "me";
+console.log(DOMSelectors.title);
+const Monkey ={title monkey}
