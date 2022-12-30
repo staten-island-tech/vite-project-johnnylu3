f21e88adf7775b7bdb9a112772e306fc3f4622cd
johnnylu3 <lujohnny517@gmail.com>
2022-12-30 00:25:51 -0500

    yuh

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,5 +1,8 @@
 const DOMSelectors = {
   display: document.querySelector(".display"),
+  hot: document.querySelector(".hot"),
+  cold: document.querySelector(".cold"),
+  healthy: document.querySelector(".healthy"),
 };
 
 export { DOMSelectors };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,84 +1,114 @@
 import "../styles/style.css";
-import "../styles/variable.css";
 import "./drinks";
 import "./dom";
 import { drinks } from "./drinks";
 import { DOMSelectors } from "./dom";
 
-const ez = drinks.filter((drink) => drink.hotorcold.includes("hot"));
+const filterHot = drinks.filter((drink) => drink.hotorcold.includes("hot"));
 
-const et = drinks.filter((drink) => drink.hotorcold.includes("cold"));
-const er = drinks.filter((drink) => drink.healthy === true);
+const filterCold = drinks.filter((drink) => drink.hotorcold.includes("cold"));
+const filterHealthy = drinks.filter((drink) => drink.healthy === true);
 
-function printHot() {
-  ez.forEach((drink) => {
+function print(r) {
+  return r.forEach((drink) => {
     console.log(drink.name);
   });
 }
 
-function printCold() {
-  et.forEach((drink) => {
-    console.log(drink.name);
-  });
-}
-
-function printHealthy() {
-  er.forEach((drink) => {
-    console.log(drink.name);
-  });
-}
-
-drinks.forEach((drink) => {
-  DOMSelectors.display.insertAdjacentHTML(
-    "beforeend",
-    `<div class= "display-card">
-  <h1 class="name">${drink.name}</h1>
-  <img class= "display-img" src="${drink.img}">
-  </div>`
-  );
-});
-
-const cardsContainer = document.querySelector(".display");
-
-document.querySelector(".light").addEventListener("click", function () {
-  document.body.classList.add("light");
-  document.body.classList.remove("dark");
-});
-
-document.querySelector(".dark").addEventListener("click", function () {
-  document.body.classList.add("dark");
-  document.body.classList.remove("light");
-});
-
-document.querySelector(".hot").addEventListener("click", function () {
-  console.clear();
-  printHot();
-  const ez1 = ez.map((drink) => {
-    return '<div class="card">' + "<span>" + drink.name + "</span>" + "</div>";
-  });
-  cardsContainer.innerHTML = ez1.join("\n");
-});
-
-document.querySelector(".cold").addEventListener("click", function () {
-  console.clear();
-  printCold();
-  const ez2 = et.map((drink) => {
-    return '<div class="card">' + "<span>" + drink.name + "</span>" + "</div>";
-  });
-  cardsContainer.innerHTML = ez2.join("\n");
-});
-
-document.querySelector(".healthy").addEventListener("click", function () {
-  console.clear();
-  printHealthy();
-  er.map((el) => {
+function showAll() {
+  DOMSelectors.display.innerHTML = ``;
+  drinks.forEach((drink) => {
     DOMSelectors.display.insertAdjacentHTML(
       "afterbegin",
-      `<div class="items">
-      <h4 class="name">${el.name}</h4>
-      <img class="food-imgs" src="${el.img}"></img>
-      <h5 class="price">${el.price}</h5>
+      `<div class="display-card">
+      <h4 class="display-name">${drink.name}</h4>
+      <img class="display-img" src="${drink.img}"></img>
+      <h5 class="display-price">$${drink.price}</h5>
       </div>`
     );
   });
+}
+
+function hot() {
+  console.clear();
+  print(filterHot);
+  filterHot.map((el) => {
+    DOMSelectors.display.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="display-card">
+      <h4 class="display-name">${el.name}</h4>
+      <img class="display-img" src="${el.img}"></img>
+      <h5 class="display-price">$${el.price}</h5>
+      </div>`
+    );
+  });
+}
+function cold() {
+  console.clear();
+  print(filterCold);
+  filterCold.map((el) => {
+    DOMSelectors.display.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="display-card">
+      <h4 class="display-name">${el.name}</h4>
+      <img class="display-img" src="${el.img}"></img>
+      <h5 class="display-price">$${el.price}</h5>
+      </div>`
+    );
+  });
+}
+
+function healthy() {
+  console.clear();
+  print(filterHealthy);
+  filterHealthy.map((el) => {
+    DOMSelectors.display.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="display-card">
+      <h4 class="display-name">${el.name}</h4>
+      <img class="display-img" src="${el.img}"></img>
+      <h5 class="display-price">$${el.price}</h5>
+      </div>`
+    );
+  });
+}
+
+showAll();
+
+document.querySelector(".light-theme").addEventListener("click", function () {
+  document.body.classList.remove("dark");
+  document.body.classList.add("light");
+});
+
+document.querySelector(".dark-theme").addEventListener("click", function () {
+  document.body.classList.remove("light");
+  document.body.classList.add("dark");
+});
+DOMSelectors.hot.addEventListener("click", function () {
+  if (DOMSelectors.display.classList.contains("hot")) {
+  } else {
+    DOMSelectors.display.innerHTML = ``;
+    DOMSelectors.display.classList.remove("healthy", "cold");
+    DOMSelectors.display.classList.add("hot");
+    hot();
+  }
+});
+DOMSelectors.cold.addEventListener("click", function () {
+  if (DOMSelectors.display.classList.contains("cold")) {
+  } else {
+    DOMSelectors.display.innerHTML = ``;
+    DOMSelectors.display.classList.remove("healthy", "hot");
+    DOMSelectors.display.classList.add("cold");
+    cold();
+  }
+});
+
+DOMSelectors.healthy.addEventListener("click", function () {
+  if (DOMSelectors.display.classList.contains("healthy")) {
+  } else {
+    DOMSelectors.display.innerHTML = ``;
+    DOMSelectors.display.classList.remove("cold", "hot");
+    DOMSelectors.display.classList.add("healthy");
+    healthy();
+  }
 });
